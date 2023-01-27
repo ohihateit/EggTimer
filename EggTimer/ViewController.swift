81fccb038c7f4154ad0b084727a8039e7d68c67e
ohihateit <whoamins33@gmail.com>
2023-01-27 21:50:50 +0300

    Initial Commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLable: UILabel!
+    var player: AVAudioPlayer!
+    var timer: Timer? = nil
+    var secondsRemaining = 60
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7] // Int values are seconds
     
-
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer?.invalidate()
+                
+        secondsRemaining = eggTimes[sender.currentTitle!]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            titleLable.text = "DONE!"
+            playSound()
+            timer?.invalidate()
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
