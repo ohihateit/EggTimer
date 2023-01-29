ab8928c7bfd609421616d40684a4210acde82337
ohihateit <whoamins33@gmail.com>
2023-01-29 14:01:29 +0300

    Add progressBar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,23 +11,36 @@ import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     var player: AVAudioPlayer!
     var timer: Timer? = nil
-    var secondsRemaining = 60
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var totalTime = 0
+    var secondsPassed = 1
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer?.invalidate()
-                
-        secondsRemaining = eggTimes[sender.currentTitle!]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+        if player != nil {
+            if player.isPlaying {
+                player.stop()
+            }
+        }
+        
+        totalTime  = eggTimes[sender.currentTitle!]!
+        titleLable.text = sender.currentTitle
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
         } else {
             titleLable.text = "DONE!"
             playSound()
